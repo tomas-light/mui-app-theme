3521ef6cb7119363705528683e1615f0eeef9ad3
Tomas_Light <pikachu-94@yandex.ru>
2020-04-24 18:51:51 +0300

    pretier autoformat

--- src/styles/InfoColorVariant.ts
+++ src/styles/InfoColorVariant.ts
@@ -1,12 +1,9 @@
 export type InfoColorVariant =
     | "success"
     | "success.text"
-
     | "info"
     | "info.text"
-
     | "warning"
     | "warning.text"
-
     | "error"
     | "error.text";
--- src/styles/MainColorVariant.ts
+++ src/styles/MainColorVariant.ts
@@ -1,12 +1,9 @@
 export type MainColorVariant =
     | "default"
     | "default.text"
-
     | "primary"
     | "primary.text"
-
     | "secondary"
     | "secondary.text"
-
     | "destructive"
     | "destructive.text";
